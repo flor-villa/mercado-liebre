d9a38a1dcec2ec7e8012734fe712e9c1ae4a52e9
flor-villa <flor.mariel.villa@gmail.com>
2023-08-22 10:08:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const PORT = 3000
+
+app.listen( PORT, () => console.log(`Server up on PORT : ${3000}`) );
+
+app.get('/' , (req, res) => {
+    res.sendFile(path.join(__dirname, './src/views/home.html'))
+});
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./src/views/register.html'));
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./src/views/login.html'));
+});
+app.post('/register',(req, res) => {
+    res.redirect('/');
+});
+app.post('/login',(req, res) => {
+    res.redirect('/');
+});
